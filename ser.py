128218718fbb27f513780dd63b91f43de7e458d9
zalivka <20828522+zalivka@users.noreply.github.com>
2025-01-08 12:27:06 +0700

    sdf

--- ser.py
+++ ser.py
@@ -54,4 +54,6 @@ def remove_background():
     return jsonify({'error': 'Invalid file type'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True, port=5000)
+    
