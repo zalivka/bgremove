1aab95639396fa092dfcf9348ddf919ebd7a98df
zalivka <20828522+zalivka@users.noreply.github.com>
2025-01-16 20:09:01 +0700

    sd

--- init.sh
+++ init.sh
@@ -8,9 +8,17 @@ echo "Downloading model file..."
 curl -L https://testitems.fra1.digitaloceanspaces.com/isnet.pth -o saved_models/isnet.pth
 
 # Create and activate virtual environment
+apt install python3.12-venv
 echo "Creating virtual environment..."
 python3 -m venv venv
 
 # Activate virtual environment
 echo "Activating virtual environment..."
 source venv/bin/activate
+
+# cache 
+export TMPDIR=/var/
+pip install -r ./requirements.txt 
+
+chmod +x run.sh
+./run.sh
