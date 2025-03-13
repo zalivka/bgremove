675327d1308380c1eb15377896c7af100a102f90
zalivka <20828522+zalivka@users.noreply.github.com>
2025-03-13 23:03:51 +0300

    wft

--- ser.py
+++ ser.py
@@ -106,7 +106,7 @@ def testcred():
     return handler.testHandler(job)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
     # app.run(debug=True, port=5000)
     
